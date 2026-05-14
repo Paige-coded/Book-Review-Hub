014e01032f9fc189efd8e4d42cc29954a2d9f86b
Paige-coded <paigerobinson2372@gmail.com>
2026-05-14 14:02:16 -0500

    Add Reviews controller with nested routes

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,32 @@
+class ReviewsController < ApplicationController
+  before_action :set_book
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = @book.reviews.new(review_params)
+    if @review.save
+      redirect_to @book, notice: "Review added!"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @review = @book.reviews.find(params[:id])
+    @review.destroy
+    redirect_to @book, notice: "Review deleted."
+  end
+
+  private
+
+  def set_book
+    @book = Book.find(params[:book_id])
+  end
+
+  def review_params
+    params.expect(review: [:body, :rating])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  resources :books
+  get "reviews/new"
+  get "reviews/create"
+  get "reviews/destroy"
+  resources :books do
+    resources :reviews, only: [:new, :create, :destroy]
+  end
   root "books#index"
 end
